d7e358d262d20f762e38a2e3fd5957e1acccbb42
Simeon A <simeonazogbonon@gmail.com>
2024-03-04 16:59:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="flex items-center justify-center w-full h-lvh">
+    <div
+      class="cards flex flex-col gap-4 transition duration-[400ms] hover:transform hover:scale-90 group"
+    >
+      <div
+        class="card bg-[#f43f5e] flex flex-col items-center justify-center text-center h-24 w-64 rounded-[10px] text-white cursor-pointer transition duration-[400ms] hover:transform group-hover:blur-md group-hover:scale-90 group-hover:hover:scale-110 group-hover:hover:blur-none"
+      >
+        <p class="text-base font-bold">Hover Me</p>
+        <p class="text-xs">Man United</p>
+      </div>
+      <div
+        class="card bg-[#3b82f6] flex flex-col items-center justify-center text-center h-24 w-64 rounded-[10px] text-white cursor-pointer transition duration-[400ms] hover:transform group-hover:blur-md group-hover:scale-90 group-hover:hover:scale-110 group-hover:hover:blur-none"
+      >
+        <p class="text-base font-bold">Hover Me</p>
+        <p class="text-xs">Chelsea</p>
+      </div>
+      <div
+        class="card bg-[#22c55e] flex flex-col items-center justify-center text-center h-24 w-64 rounded-[10px] text-white cursor-pointer transition duration-[400ms] hover:transform group-hover:blur-md group-hover:scale-90 group-hover:hover:scale-110 group-hover:hover:blur-none"
+      >
+        <p class="text-base font-bold">Hover Me</p>
+        <p class="text-xs">Sporting Club</p>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      /*scale: {
+        '90': '.9',
+      },
+      blur: {
+        '10': '10px',
+      }*/},
+  },
+  plugins: [],
+}
+
